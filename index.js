bcc9ed93009f53fb89665edf00ea64ce4da6fd19
Bookcliff <bookcliff76@gmail.com>
2022-07-13 09:00:54 -0500

    second commit

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const DiscordJS = require('discord.js');
 
 require('dotenv').config();
 
-const client = new DiscordJS.Client()
+const client = new DiscordJS.Client();
 
 client.on('ready', () => {
     console.log('The bot is ready')
-})
-sldjf
-client.login(process.env.TOKEN)
+});
+
+client.login(process.env.TOKEN);
 
